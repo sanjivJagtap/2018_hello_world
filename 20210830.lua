42b3d521eebb65a7134f9e3ee365bbd27b512562
Sanjiv <sanjivjagtap@gmail.com>
2021-08-30 14:44:28 -0400

    Update 20210830.lua

--- 20210830.lua
+++ 20210830.lua
@@ -1,1 +1,3 @@
 -- 08 30 2021. I just created a new file. WHen I added a ".lua" suffix, a "commit new file" pop up came, saying "Create 20210830.lua", and to "Add an optional extended description..." I see the options to commit directly to the master branch, or also create a new branch
+
+--However, once added, this isn't exactly an editor friendly version.  Meaning, I wouldn't use this to work on the daily. Unless of course I did this before adding in the suffix. 
